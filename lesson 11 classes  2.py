57e864fb7e5beed2adaa762c069949399b758c8e
Vlad <fedchenkovlad1899@gmail.com>
2026-02-11 18:33:53 +0300

    add ex3-4 lesson 11 classes2

--- lesson 11 classes  2.py
+++ lesson 11 classes  2.py
@@ -56,3 +56,108 @@ sender_1 = Sender()
 sender_1.send_by_method(BySMS('9379992'),'"You have new message!"')
 sender_1.send_by_method(ByTelegram('ddanil'),'"You have new message!"')
 sender_1.send_by_method(ByEmail('danilkolbasenko@mail.ru'),'"You have new message!"')
+
+'''
+3.Создайте класс User с ролью. Реализуйте policy-класс, определяющий доступ к методам. 
+Создайте декоратор, который проверяет право пользователя на выполнение метода. 
+Продемонстрируйте разрешённый и запрещённый доступ без if внутри метода.
+'''
+class User():
+    def __init__(self, name: str, access_policy: str):
+        self.name = name
+        self.access_policy = access_policy
+
+
+class Policy():
+    def admin(user: User):
+        return user.access_policy == "admin"
+
+    def default_user(user: User):
+        return user.access_policy in ["default_user", "admin"]
+
+
+def chek_policy(policy_f):
+    def decorator(func):
+        def wrapper(user, *args, **kwargs):
+            while not policy_f(user):
+                raise PermissionError(f"у {user.name} нет доступа ")
+            return func(user, *args, **kwargs)
+
+        return wrapper
+
+    return decorator
+
+
+@chek_policy(Policy.admin)
+def delete_inf(user: User):
+    print(f'Информация удалена {user.name}')
+
+
+@chek_policy(Policy.default_user)
+def read(user: User):
+    print(f'читайте:) {user.name}')
+
+
+user = User('Vova', "default_user")
+admin = User('Administrator', "admin")
+
+delete_inf(admin)
+read(admin)
+read(user)
+try:
+    delete_inf(user)
+except PermissionError as e:
+    print(e)
+
+'''
+4.	Создай класс BankAccount, который имеет закрытый баланс __balance. Позволяет пополнять deposit
+и снимать withdraw деньгию. Не позволяет снимать больше, чем есть на счету. Вводит суточный лимит снятия
+ (например, 5000). Сделайте ограничение по транзакциям, не более 3 – х
+'''
+class BankAccount:
+    def __init__(self, balance: float = 0):
+        self.__balance = balance
+        self.daily_limit = 5000
+        self.withdrawals_today = 0
+        self.max_withdrawals = 3
+        self.withdrawals_count = 0
+
+    def deposit(self, amount: float):
+        if amount < 0:
+            print('Пополнение должно быть положительным')
+            return
+        self.__balance += amount
+        print(f'Cчет пополнен на {amount}.Баланс: {self.__balance}')
+
+    def withdraw(self, amount: float):
+        if amount > self.__balance:
+            print('Недостаточно средств')
+            return
+        if self.withdrawals_today + amount >= self.daily_limit:
+            print(f'превышен лимит снятия.Доступно {self.daily_limit - self.withdrawals_today} ')
+            return
+        if self.withdrawals_count >= self.max_withdrawals:
+            print('превышен лимит операций')
+            return
+        self.__balance -= amount
+        self.withdrawals_today += amount
+        self.withdrawals_count += 1
+        print(f'Снято: {amount}.Баланс: {self.__balance}')
+
+    def get_balance(self):
+        print(f"Баланс: {self.__balance}")
+        return self.__balance
+
+
+bank = BankAccount()
+bank.deposit(10000)
+bank.withdraw(4000)
+bank.withdraw(100)
+bank.withdraw(1000)
+bank.deposit(1000)
+bank.withdraw(100)
+bank.withdraw(170)
+bank.withdraw(100)
+bank.withdraw(100)
+bank.withdraw(100)
+bank.get_balance()
